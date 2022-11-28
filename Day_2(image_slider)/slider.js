9af9d42b86016e129e595f6251294dd07ace56be
sumit-krk <kushakk007@gmail.com>
2022-11-28 19:48:48 +0530

    fixed a small bug in image slider

--- Day_2(image_slider)/slider.js
+++ Day_2(image_slider)/slider.js
@@ -1,12 +1,20 @@
-let flag=0;
+var flag=0;
 slider_image(flag);
-
-function slider_image(flag){
+function slider_image(num){
     let slider=document.getElementsByClassName('Image_slider')
+    if(num==slider.length){
+        flag=0;
+        num=0;
+    }
+    if(num==-1){
+        flag=3;
+        num=3;
+    }
+    console.log("slider_flag",flag,"slider length",slider.length)
     for(let x of slider){
         x.style.display="none";  
     }
-    slider[flag].style.display="block";
+    slider[num].style.display="block";
 }
 
 function previous_image(x){
