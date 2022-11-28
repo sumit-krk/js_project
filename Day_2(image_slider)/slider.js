f2a710ba425507c3e09fb80d6d45d297d3b1f59a
sumit-krk <kushakk007@gmail.com>
2022-11-28 12:56:40 +0530

    slider Implemented

--- Day_2(image_slider)/slider.js
+++ Day_2(image_slider)/slider.js
@@ -1,2 +1,20 @@
-let slider=document.getElementsByClassName('Image_slider')
-console.log(slider);
+let flag=0;
+slider_image(flag);
+
+function slider_image(flag){
+    let slider=document.getElementsByClassName('Image_slider')
+    for(let x of slider){
+        x.style.display="none";  
+    }
+    slider[flag].style.display="block";
+}
+
+function previous_image(x){
+    flag=flag-1;
+    slider_image(flag)
+}
+
+function next_image(y){
+    flag=flag+1;
+    slider_image(flag)
+}
