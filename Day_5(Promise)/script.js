a0503f01f739636aadb664fcea59ce3bf40fd9e4
sumit-krk <kushakk007@gmail.com>
2022-12-02 18:16:22 +0530

    handle promise in api

--- Day_5(Promise)/script.js
+++ Day_5(Promise)/script.js
@@ -1,13 +1,22 @@
-let data=new Promise((resolved,reject)=>{
-    setTimeout(()=>{
-        reject("Task Rejected")
-    },2000)
-})
+// How to make promise
+// let data=new Promise((resolved,reject)=>{
+//     setTimeout(()=>{
+//         reject("Task Rejected")
+//     },2000)
+// })
 
-data.then((item)=>{
-    console.log(item)
-}).catch((item)=>{
-    console.log(item)
-})
+// data.then((item)=>{
+//     console.log(item)
+// }).catch((item)=>{
+//     console.log(item)
+// })
 
-console.log("Hello");
+// console.log("Hello");
+
+// how to handle promise in api.
+let data = fetch("https://jsonplaceholder.typicode.com/todos/1");
+data.then((res)=>{
+    return res.json();
+}).then((result)=>{ // promise chaning
+    console.log(result);
+})
