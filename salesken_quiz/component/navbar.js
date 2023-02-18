004d291193567f41a2bfd8a43c2a974fe2cabca8
sumit-krk <kushakk007@gmail.com>
2023-02-19 03:45:44 +0530

    show user name in navbar

--- salesken_quiz/component/navbar.js
+++ salesken_quiz/component/navbar.js
@@ -1,8 +1,12 @@
+const data=JSON.parse(localStorage.getItem("login"))
+console.log("data",data)
 function navbar(arg)
 {
     return `<div class='navBar'>
-        <a href="index.html">Quiz Apps</a></h1>
-        ${arg=='signUp'?`<a href="signup.html">Sign-up</a>`:`<a href="login.html">Login</a>`}
+        <a href="index.html">Quiz Apps</a>
+        ${
+            data && data.status==1?`<a>${data.name}</a>`:`${arg=='signUp'?`<a href="signup.html">Sign-up</a>`:`<a href="login.html">Login</a>`}`
+        }
     </div>`;
 }
 export default navbar
