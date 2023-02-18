9368a2e9b2f5d39f7bc06cc825f292da94dc1f42
sumit-krk <kushakk007@gmail.com>
2023-02-19 03:44:24 +0530

    question data file

---
+++ salesken_quiz/index.js
@@ -0,0 +1,86 @@
+let all_question=[
+    {
+        question: "1. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "2. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "3. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "4. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "5. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "6. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "7. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "8. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "9. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    },
+    {
+        question: "10. The full form of CSS is:",
+        a:"Cascading Style Sheets",
+        b:"Coloured Special Sheets",
+        c:"Color and Style Sheets",
+        d:"None of the above",
+        ans:"ans1"
+    }
+]
+
+const onbody_load=()=>{
+    localStorage.setItem("question",JSON.stringify(all_question));
+}
