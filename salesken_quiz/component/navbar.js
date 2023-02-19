782d0c26717b1bd2daeabf3b2440560fb38cb83f
sumit-krk <kushakk007@gmail.com>
2023-02-19 06:31:12 +0530

    made some changes in navbar

--- salesken_quiz/component/navbar.js
+++ salesken_quiz/component/navbar.js
@@ -1,11 +1,12 @@
+// navbar component
+
 const data=JSON.parse(localStorage.getItem("login"))
-console.log("data",data)
 function navbar(arg)
 {
     return `<div class='navBar'>
         <a href="index.html">Quiz Apps</a>
         ${
-            data && data.status==1?`<a>${data.name}</a>`:`${arg=='signUp'?`<a href="signup.html">Sign-up</a>`:`<a href="login.html">Login</a>`}`
+            data && data.status==1?`<div class='handle_icon'><a>${data.name}</a><i class="fa fa-sign-out" onclick='logOut()'></i></div>`:`${arg=='signUp'?`<a href="signup.html">Sign-up</a>`:`<a href="login.html">Login</a>`}`
         }
     </div>`;
 }
