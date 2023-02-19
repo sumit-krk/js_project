80e82b43c63de8f5ed24a0d5bff5a10e34e6b1df
sumit-krk <kushakk007@gmail.com>
2023-02-19 09:06:13 +0530

    made some changes

--- salesken_quiz/component/navbar.js
+++ salesken_quiz/component/navbar.js
@@ -6,7 +6,7 @@ function navbar(arg)
     return `<div class='navBar'>
         <a href="index.html">Quiz Apps</a>
         ${
-            data && data.status==1?`<div class='handle_icon'><a>${data.name}</a><i class="fa fa-sign-out" onclick='logOut()'></i></div>`:`${arg=='signUp'?`<a href="signup.html">Sign-up</a>`:`<a href="login.html">Login</a>`}`
+            data && data.status==1?`<div class='handle_icon'><a>${data.name}</a><i class="fa fa-sign-out" id='logout' onclick='logOut()'></i></div>`:`${arg=='signUp'?`<a href="signup.html">Sign-up</a>`:`<a href="login.html">Login</a>`}`
         }
     </div>`;
 }
