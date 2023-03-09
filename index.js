e759e306abd17144ea31ed4086d463dfc3b3a5c5
sumit-krk <kushakk007@gmail.com>
2023-03-09 23:23:11 +0530

    sepread operater

--- index.js
+++ index.js
@@ -2,7 +2,16 @@
     var a=10;
     let b=20;
     const c=30;
+    console.log(a)
 }
 console.log(a)
-console.log(b)
-console.log(c)
+
+function add(a,...b)
+{
+    console.log(b);
+}
+add(1,2,3,4,5)
+
+const arr=[1,2,3,4,5]
+
+const arr2=[...arr]
